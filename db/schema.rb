79a455ea7697a696a3c7bf36e086a750b10eef0c
Daniel Ran <danielr1735@gmail.com>
2018-07-09 22:11:34 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 6) do
+ActiveRecord::Schema.define(version: 5) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 6) do
   end
 
 # Could not dump table "characters" because of following StandardError
-#   Unknown type 'catchphrase' for column 'string'
+#   Unknown type '' for column 'string'
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
