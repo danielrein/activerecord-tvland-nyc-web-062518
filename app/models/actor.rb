807b14ba0959e841b34dd1d441d9c93d8344ce08
Daniel Ran <danielr1735@gmail.com>
2018-07-09 22:28:10 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,13 +2,15 @@ class Actor < ActiveRecord::Base
   has_many :chracters
   has_many :shows, through: :characters
 
-
-
   def full_name
     "#{first_name} #{last_name}"
   end
 
   def list_roles
-    Character.all.select { |c| c.actor == self }
+    Character.where(actor: self)
+  end
+
+  def characters
+    Character.where(actor: self)
   end
 end
