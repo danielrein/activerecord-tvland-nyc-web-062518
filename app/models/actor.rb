54a4eb2d59a47e1fbb5304c518220837d1f28733
Daniel Ran <danielr1735@gmail.com>
2018-07-09 22:30:18 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,9 +6,9 @@ class Actor < ActiveRecord::Base
     "#{first_name} #{last_name}"
   end
 
-  # def list_roles
-  #   Character.where(actor: self)
-  # end
+  def list_roles
+    Character.where(actor: self).map { |c| "#{c} - #{c.show}"}
+  end
 
   def characters
     Character.where(actor: self)
